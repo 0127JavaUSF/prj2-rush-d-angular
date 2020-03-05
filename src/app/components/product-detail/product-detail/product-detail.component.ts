f6c19bd4da1462f85c36531766ad5ec848365ffc
Kirsaly Heredia <60357183+kirsalyHeredia@users.noreply.github.com>
2020-03-04 21:55:05 -0500

    home done

---
+++ src/app/components/product-detail/product-detail/product-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  constructor() { }
+
+
+  quantity: number = 0;
+
+  addQty(){
+    this.quantity += 1;
+  }
+
+  subQty(){
+    if (this.quantity < 1){
+      this.quantity = 0;
+    } else{
+      this.quantity -= 1;
+    }
+    
+  }
+  
+  ngOnInit(): void {
+  }
+
+}
