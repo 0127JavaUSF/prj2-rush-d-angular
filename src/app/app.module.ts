1ef6752d2efb435c8337c2e4c8fc3c00ca1989cb
Joshua Ventura <jventura11@hawkmail.hccfl.edu>
2020-03-06 13:05:45 -0500

    Added Header and partial layout of order-summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products/products.component';
+import { CartViewComponent } from './components/cart-view/cart-view.component';
+import { CartOrderSummaryComponent } from './components/cart-order-summary/cart-order-summary.component';
+import { ProductCartComponent } from './components/product-cart/product-cart.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    CartViewComponent,
+    CartOrderSummaryComponent,
+    ProductCartComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/cart-order-summary/cart-order-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-order-summary',
+  templateUrl: './cart-order-summary.component.html',
+  styleUrls: ['./cart-order-summary.component.css']
+})
+export class CartOrderSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
