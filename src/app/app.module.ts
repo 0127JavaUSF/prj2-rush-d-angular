8df1b9cc0996cf599311de8c053c447e54f08412
Kirsaly Heredia <60357183+kirsalyHeredia@users.noreply.github.com>
2020-03-07 16:51:40 -0500

    blah

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './components/products/products/products.component';
+import { ProductsComponent } from './components/products/products.component';
 
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-import { HomeComponent } from './components/home/home/home.component';
-import { CateringComponent } from './components/catering/catering/catering.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail/product-detail.component';
+import { HomeComponent } from './components/home/home.component';
+import { CateringComponent } from './components/catering/catering.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 
 @NgModule({
@@ -18,7 +18,7 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
     AppComponent,
     ProductsComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
 
     HomeComponent,
     CateringComponent,
