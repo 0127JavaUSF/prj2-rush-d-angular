48ef84dc804ac69736d5394ec8fe99857e17af10
Khanh <knguye15.ccp@gmail.com>
2020-03-07 16:34:40 -0500

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products/products.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { SummarylistComponent } from './components/summarylist/summarylist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    SummaryComponent,
+    SummarylistComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/summarylist/summarylist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-summarylist',
+  templateUrl: './summarylist.component.html',
+  styleUrls: ['./summarylist.component.css']
+})
+export class SummarylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
